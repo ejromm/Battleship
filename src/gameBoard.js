faa8fc67a9f5c76a3632e234ccc3ad1967bcecef
Ethan Romm <ejromm@gmail.com>
2023-04-02 20:54:10 -0400

    completed non-computer gameboard methods

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,21 +1,81 @@
+import { Ship } from "./ship";
 class GameBoard {
     constructor(board) { 
         this.board = [];
         this.initBoard();
+        this.ships = []; 
+        this.gameOver = false; 
     }
     initBoard() {
         for(let i = 0 ; i < 10 ; i++) {
-          for(let j = 0; j < 10 ; j++) {
-                this.board.push({ship: false, isHit: false});
+                this.board.push([]);
+          for(let j = 0; j < 100 ; j++) {
+                this.board[i].push({ship: null, isHit: false});
 
             }
         }
     }; 
-    recieveAttack(location) {
-        this.board[location].isHit = true; 
+    recieveAttack(col, row) {
+        if (this.board[col][row].isHit === false) {
+            this.board[col][row].isHit = true; 
+            if(this.board[col][row].ship !== null) {
+              let currentShip =  this.ships.find((ship) => ship.name === this.board[col][row].ship.name); 
+              currentShip.hit();
+              currentShip.isSunk();
+            }
+        }
+       return false; 
     }
-    checkIfAttackHit(location) {
-        return this.board[location].ship; 
+    
+    pushShip(isVertical, length, name, col, row) {
+            if (this.canBePlaced(isVertical, length, col,row)) {
+               if(isVertical) {
+                  for(let i = 0 ; i < length; i++) {
+                    this.board[col + i][row].ship = new Ship(length, name); 
+                    this.ships.push(new Ship(length, name));
+                  }
+
+
+               } else {
+                 for(let i = 0 ; i < length; i++) {
+                    this.board[col][row + i].ship = new Ship(length, name); 
+                    this.ships.push(new Ship(length, name));
+                 }
+
+
+               }
+
+            }
+
+        return false; 
     }
-}
+    canBePlaced(isVertical, length, col, row) {
+        if (row < 0 || row > 9 || col < 0 || col > 9) {
+            return false; 
+        }
+       for (let i = 0 ; i < length; i++) {
+            if(isVertical) {
+                if(col + i > 9 ) {
+                    return false; 
+                }
+                if (this.board[col + i][row].ship !== null) {
+                    return false; 
+                }
+                
+
+
+            } else if(!isVertical) {
+                if( row + i > 9 ) {
+                    return false; 
+                }
+                if(this.board[col][row + i].ship !== null) {
+                    return false;
+                }
+             
+
+            }
+       }
+       return true; 
+    }; 
+    
 export { GameBoard };
--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -1,22 +1,94 @@
 import { GameBoard } from "../src/gameBoard";
+import { Ship } from "../src/ship";
 describe('gameboard', () => {
     const newBoard = new GameBoard; 
     describe('inital setup', () => {
         test('newly created board has a 10 x 10 board built in', () => {
-            expect(newBoard.board.length).toBe(100);
+            expect(newBoard.board.length).toBe(10);
+        }); 
+    }); 
+    describe('can be placed', () => {
+        const newBoardTwo = new GameBoard; 
+        test('if row input is out of bounds return false', () => {
+            expect(newBoardTwo.canBePlaced(false, 4, 12, 0)).toBe(false);
+    
+        });
+        test('if col input if out of bounds return false', () => {
+            expect(newBoardTwo.canBePlaced(false, 4, 0 ,12)).toBe(false);
+        });
+        newBoardTwo.board[0][4].ship = true; 
+        newBoardTwo.board[2][2].ship = true; 
+        test('if is vertical and any spots are already taken', () => {
+            expect(newBoardTwo.canBePlaced(true, 4, 0 , 2)).toBe(false);
+        }); 
+        test("if isn't vertical and any spots are already taken", () => {
+            expect(newBoardTwo.canBePlaced(false, 4, 0 , 2)).toBe(false);
+        })
+        test('if is vertical and any potential spots are out of bounds', () => {
+            expect(newBoardTwo.canBePlaced(true, 5, 7, 5)).toBe(false); 
+        }); 
+        test("if isn't vertical and any potential spots are out of bounds", () => {
+            expect(newBoardTwo.canBePlaced(false, 5, 3, 7)).toBe(false);
+        });
+        test('if is vertical and any spots are not already taken', () => {
+            expect(newBoardTwo.canBePlaced(true, 4, 0 , 5)).toBe(true);
+        }); 
+        test("if isn't vertical and any spots are not already taken", () => {
+            expect(newBoardTwo.canBePlaced(false, 4, 1 , 2)).toBe(true);
+        })
+        test('if is vertical and any potential spots are not out of bounds', () => {
+            expect(newBoardTwo.canBePlaced(true, 1, 7, 5)).toBe(true); 
+        }); 
+        test("if isn't vertical and any potential spots are not out of bounds", () => {
+            expect(newBoardTwo.canBePlaced(false, 1, 3, 7)).toBe(true);
+        });
+    }); 
+    describe('push ship', () => {
+        const newBoardThree = new GameBoard;
+        
+        test('prevents invalid placement', () => {
+            expect(newBoardThree.pushShip(false, 5, 'Battleship',  3, 7)).toBe(false);
+        });
+        newBoardThree.pushShip(true, 2, 'Battleship', 7, 5);
+        const newShip = new Ship(2, 'Battleship');
+        test('changes ship value to a ship object at each point', () => {
+            expect(newBoardThree.board[7][5].ship).toStrictEqual(newShip);
+        }); 
+        test('ship value at point has same name as passed in', () => {
+            expect(newBoardThree.board[7][5].ship.name).toBe('Battleship');
+        })
+        test('ships array takes in new ship after ship is pushed to board', () => {
+            expect(newBoardThree.ships[0]).toStrictEqual(newShip); 
         }); 
     }); 
     describe('recieve attack', () => {
-        newBoard.recieveAttack(4);
+        const newBoardFour = new GameBoard; 
+        newBoardFour.recieveAttack(0, 4);
         test('attack accurately changes index hit to true', () => {
-            expect(newBoard.board[4].isHit).toBe(true);
+            expect(newBoardFour.board[0][4].isHit).toBe(true);
         });
-        test('check if attack hit a ship', () => {
-            expect(newBoard.board[4].ship).toBe(false);
-        });
-        
+        newBoardFour.pushShip(true, 2, 'Battleship', 7, 5);
+        const newShip = new Ship(2, 'Battleship'); 
+        newBoardFour.recieveAttack(8, 5); 
+        const currentShip = newBoardFour.ships.find((ship) => ship.name === newShip.name);
+        test('ship in ship array recieves an attack on the board', () => {
+            expect(currentShip.hits).toBe(1); 
+        })
     }); 
-   
+    describe('check if game over', () => {
+        const newBoardFive = new GameBoard;
+        newBoardFive.pushShip(true, 2, 'Battleship', 7, 5);
+        newBoardFive.pushShip(true, 2, 'Battleship-Two', 7, 7);
+        newBoardFive.recieveAttack(7, 5);
+        newBoardFive.recieveAttack(8, 5);
+        newBoardFive.recieveAttack(7, 7);
+        newBoardFive.recieveAttack(8, 7);
+        newBoardFive.checkIfGameOver(); 
+        test('if all boats are sunk, game over should be true', () => {
+            expect(newBoardFive.gameOver).toBe(true);
+        })
+
+    })
        
  
    
